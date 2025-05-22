710988582ef840cde4fc0b9a7ccf3778aa22cb51
Paguiel <119151331+paguielng@users.noreply.github.com>
2025-05-22 13:06:29 +0200

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -33,7 +33,7 @@ const Header: React.FC<HeaderProps> = ({ toggleSidebar }) => {
   <div className="h-8 w-8">
     <img src="/logo-light.svg" alt="shoppinglist logo" />
   </div>
-  <span className="hidden md:inline-block font-bold text-xl">Shoppland Wiki</span>
+  <span className="hidden md:inline-block font-bold text-xl">ShoppList Blog</span>
 </Link>
 
         </div>
