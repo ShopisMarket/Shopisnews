33d3746bab3353fc8aaf8352d23e469de7d573a5
Paguiel <119151331+paguielng@users.noreply.github.com>
2025-07-30 18:31:37 +0200

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -85,7 +85,7 @@ const HomePage: React.FC = () => {
     {
       title: 'Features',
       description: 'Innovative features that make ShoppingListApp the smartest way to shop.',
-      icon: monitor-smartphone,
+      icon: Shield,
       path: '/Features',
       articleCount: 9
     },
@@ -99,7 +99,7 @@ const HomePage: React.FC = () => {
     {
       title: 'Community Forum',
       description: 'Join our vibrant community of shoppers to share tips, get help, and participate in our food donation initiatives.',
-      icon: users-round,
+      icon: Shield,
       path: '/Community',
       articleCount: 9
     }
