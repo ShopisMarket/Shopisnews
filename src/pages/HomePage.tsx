43e365795568952683f9e8a188d69be53bb92355
Paguiel <119151331+paguielng@users.noreply.github.com>
2025-07-30 18:25:09 +0200

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -78,7 +78,7 @@ const HomePage: React.FC = () => {
     {
       title: 'Services',
       description: 'Designed to simplify your shopping experience from planning to checkout.',
-      icon: Wrench,
+      icon: ShoppingBag,
       path: '/ServicesPage',
       articleCount: 9
     },
